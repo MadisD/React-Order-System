263318c911514f6c7d9b3ac5368f275ab7f728ae
MadisD <madisdreifeld@gmail.com>
2016-08-07 01:01:17 +0300

    Added build files and Client, Product classes

---
+++ src/main/java/hello/Client.java
@@ -0,0 +1,18 @@
+package hello;
+
+/**
+ * Created by Madis on 06.08.2016.
+ */
+public class Client {
+    private int securityNr;
+    private String firstName;
+    private String lastName;
+    private int phoneNr;
+    private String country;
+    private String address;
+
+
+
+
+
+}
---
+++ src/main/java/hello/Product.java
@@ -0,0 +1,15 @@
+package hello;
+
+/**
+ * Created by Madis on 06.08.2016.
+ */
+public class Product {
+    private int barcode;
+    private String name;
+    private double price;
+    private String description;
+    private String releaseDate;
+
+
+
+}
